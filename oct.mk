7ea9578ae98d92c549da30e37a60d16a5dd183f7
Carl Windhausen <cjwindhausen@gmail.com>
2013-10-10 11:36:37 -0400

    Update to 4.3.1 r1

--- oct.mk
+++ oct.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/oct/config/nfc_enhanced.mk)
 # Inherit some common OCT stuff.
 $(call inherit-product, vendor/oct/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltexx TARGET_DEVICE=jfltexx BUILD_FINGERPRINT="samsung/oct_jfltexx/jfltexx:4.3/JLS36G/user/release-keys" PRIVATE_BUILD_DESC="jfltexx-user 4.3 JLS36G release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltexx TARGET_DEVICE=jfltexx BUILD_FINGERPRINT="samsung/oct_jfltexx/jfltexx:4.3.1/JLS36I/user/release-keys" PRIVATE_BUILD_DESC="jfltexx-user 4.3.1 JLS36I release-keys"
 
 PRODUCT_DEVICE := jfltexx
 PRODUCT_NAME := oct_jfltexx
